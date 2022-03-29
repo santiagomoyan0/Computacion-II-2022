edc322af14745c7dd4bf9cb263f88c169cc9646c
Santiago Moyano <santiago98moy@gmail.com>
2022-03-29 18:11:58 -0300

    segundo commit ej1y2

--- ej1y2/getopt.py
+++ ej1y2/getopt.py
@@ -4,7 +4,27 @@ import getopt
 import sys
 
 (opt,arg) = getopt.getopt(sys.argv[1:], 'o:n:p:')
-if len(opt) != 3:
-    print("enter correctly the number of parameters")
-    exit()
 
+for (op,ar) in opt:
+    if op == '-o':
+        operador = ar
+    if op == '-n':
+        numero1 = int(ar)
+    if op == '-m':
+        numero2 = int(ar)
+
+def calculadora(operador,numero1,numero2):
+    if operador == '+':
+        print(numero1+numero2)
+    elif operador == '-':
+        print(numero1-numero2)
+    elif operador == '*':
+        print(numero1*numero2)
+    elif operador == '/':
+        print(numero1/numero2)
+
+if operador in ["+","-","*",""]:
+    calculadora(operador,numero1,numero2)
+else:
+    print ("El operador es incorrecto, usar +, -, *, /")
+    
