fe9eb050f5f39f18c2136ff5cfb1eecf03e973ba
Santiago Moyano <santiago98moy@gmail.com>
2022-03-29 20:16:13 -0300

    tercer commit ej1y2

--- ej1y2/argparse.py
+++ ej1y2/argparse.py
@@ -2,4 +2,13 @@
 
 import argparse
 
-parser = argparse
+
+parser = argparse.ArgumentParser(description="ejemplo parser")
+
+parser.add_argument("-i", "--origen_archivo", type=str, required=True, help="nombre del origen")
+
+parser.add_argument("-o", "--destino_archivo", type=str, default=1024, help="nombre del destino")
+
+args = parser.parse_args()
+
+
