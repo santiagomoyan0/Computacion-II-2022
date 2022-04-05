e15784979973a0290c03598efd5723c1e171e8ca
Santiago Moyano <santiago98moy@gmail.com>
2022-04-05 11:21:55 -0300

    segundo commit subprocess

--- subprocess/s_popen.py
+++ subprocess/s_popen.py
@@ -4,3 +4,15 @@ import getopt
 import datetime as dt
 
 opt,arg = getopt.getopt(sys.argv[:1], 'c:f:l:')
+
+log_file = ''
+command = ''
+output_file = ''
+
+for (op,ar) in opt:
+    if op == '-c':
+        command = ar
+    elif op == '-f':
+        output_file = open(ar, "a")
+    elif op == '-l':
+        log_file = ar
