8f2a2c13f262796ba9695647db25e1ace588d48e
Santiago Moyano <santiago98moy@gmail.com>
2022-04-05 11:27:48 -0300

    tercer commit subprocess

--- subprocess/s_popen.py
+++ subprocess/s_popen.py
@@ -16,3 +16,6 @@ for (op,ar) in opt:
         output_file = open(ar, "a")
     elif op == '-l':
         log_file = ar
+
+process = sp.Popen([command], stdout=output_file, stderr=sp.PIPE, shell=True, universal_newlines=True)
+error = process.communicate()[1]
